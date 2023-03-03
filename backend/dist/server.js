22796ba348a7a423d10bd754561170006bbdc030
Richard Tumaneng <ren.tumaneng@gmail.com>
2023-03-02 16:45:19 -0800

    Open AI integration in server.ts

---
+++ backend/dist/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
+
+console.log(process.env.OPENAI_API_KEY)
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 app.use(cors());
 
@@ -13,6 +21,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+app.post('/',
+
+)
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
